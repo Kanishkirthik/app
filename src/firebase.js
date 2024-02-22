e53e07c3207287629a8a20a5045db819d22803aa
Kanish Kirthik K <129273259+Kanishkirthik@users.noreply.github.com>
2024-02-22 11:37:00 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyCgvN5s4ejojbxQ26F2eN-KrAahvW7UOf8",
@@ -10,7 +10,7 @@ const firebaseConfig = {
     appId: "1:957539690071:web:8ea099994dbac3b55e2512",
     measurementId: "G-SW8PHQ7VXW"
   };
-const app=firebase.initializeApp(firebaseConfig);
+const app=initializeApp(firebaseConfig);
 export const auth=app.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const googleAuthProvider = new app.auth.GoogleAuthProvider();
 export default app;  
